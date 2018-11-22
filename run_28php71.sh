644c50dd1955c10916c956e5ffdadbd880af3c44
Serhii Nikolaichuk <serh@limit.com.ua>
2018-11-22 19:00:21 +0200

    php.ini change

--- run_28php71.sh
+++ run_28php71.sh
@@ -8,8 +8,8 @@ docker run  -d \
 	    -e TZ="$TZ" \
             --link mysql57:db \
             -v ${PROJECT_DIR}:/usr/share/nginx \
-            -v ${PWD}/inc/php.ini:/usr/local/etc/php/php.ini \
 	    -v ${PWD}/inc/crontab:/etc/cron.d/crontab \
+            -v ${PWD}/inc/php.ini:/usr/local/etc/php/php.ini \
             $IMAGE
 #docker  exec -it ${CONTAINER} bash -c "/etc/init.d/cron start"
 #docker  exec -it ${CONTAINER} bash -c "crontab -u root /etc/cron.d/crontab"
