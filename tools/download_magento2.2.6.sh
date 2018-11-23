a43957b9dfdbf75f5d7c9f114b2a782b4526562a
Serhii Nikolaichuk <serh@limit.com.ua>
2018-11-23 22:24:56 +0200

    rm mag

--- tools/download_magento2.2.6.sh
+++ tools/download_magento2.2.6.sh
@@ -2,8 +2,5 @@
 curl -o mag.tar.gz -L https://github.com/magento/magento2/archive/2.2.6.tar.gz
 tar zxvf mag.tar.gz
 rm mag.tar.gz
-if [  -f "mag" ]; 
-then
-mv mag mag_back
-fi
+rm -rf mag 
 mv magento2-2.2.6 mag
